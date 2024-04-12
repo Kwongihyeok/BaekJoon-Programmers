47385bb96ac3ce67e96421d282c7f6ab38a050c2
Twixtor <83451619+Kwongihyeok@users.noreply.github.com>
2024-04-12 13:56:31 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 196 ms, Memory: 17568 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String string;
+		string = sc.next();
+		int length;
+		length = string.length();
+		System.out.println(length);
+	}
+}
